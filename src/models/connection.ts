577a4f74938550d2943e58924d2e9f8b7567233e
João Victor Arouca <joao.silveira@estudante.ufla.br>
2022-06-11 19:55:17 -0300

    feat: rename .env

--- src/models/connection.ts
+++ src/models/connection.ts
@@ -4,9 +4,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const connection = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost' || '127.0.0.1',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || 'password',
+  host: process.env.MYSQL_HOST || 'localhost' || '127.0.0.1',
+  user: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || 'password',
   // Referência: https://stackoverflow.com/questions/32100434/mysql-returns-full-datetime-string-on-select-query-when-column-type-is-date
 });
 
